22307fcddc86e7c93216aa2494d51cbb5141becc
brandonxjerome <jeromeyumang@live.com>
2015-11-29 15:47:43 -0500

    11/29/15
    Files updated, background music added, game functions started, still
    need a lot of work to do.

---
+++ game-assignment/js/main.js
@@ -0,0 +1,74 @@
+// Money clicking function //
+
+$(document).ready(function() {
+
+   $('div').click(function() {
+       $(this).toggle(700);
+
+   }); 
+});
+
+// Level One text fade out //
+
+$(document).ready(function() {
+
+    $(".l1").fadeOut(3500);
+
+});
+
+// Main Menu blinking function //
+
+$(document).ready(function() {
+
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+    $(".begin").fadeOut(1000);
+    $(".begin").fadeIn(1000);
+
+});
+
+// Start blinking function //
+
+$(document).ready(function() {
+	$(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+    $(".start").fadeOut(1000);
+    $(".start").fadeIn(1000);
+});
+
+
+
