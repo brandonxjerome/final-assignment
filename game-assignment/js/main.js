24c2b1a136275d8b07f9a10b4a3c46c923a7179b
brandonxjerome <jeromeyumang@live.com>
2015-12-05 01:48:39 -0500

    12/4/15
    In class today, got help with having the dollar bills popping out of
    random places, background music continuously playing throughout pages,
    added a sound effect for when the money is clicked, cleaner layout.

--- game-assignment/js/main.js
+++ game-assignment/js/main.js
@@ -2,17 +2,32 @@
 
 $(document).ready(function() {
 
-   $('div').click(function() {
-       $(this).toggle(700);
+var w = $(window).width();
+var h = $(window).width();
+
+    $('.money').each(function() {
+        $(this).css('left', Math.random() * w).css('top', Math.random() * h)
+    })
+
+
+   $('.money').click(function() {
+       $(this).toggle(600);
 
    }); 
-});
 
-// Level One text fade out //
 
-$(document).ready(function() {
+    // Level One text fade out //
 
-    $(".l1").fadeOut(3500);
+    $(".l1").fadeOut(3000);
+
+    $('a[data-page]').on('click', function(e) {
+
+        var go = $(this).attr('data-page');
+        $('.page').fadeOut(400).filter('#'+go).fadeIn(400);
+    });
+
+    
+
 
 
 });
@@ -32,6 +47,14 @@ $(function () {
     }
 
     fadeInOut();
+});
+
+var cash = document.getElementById('cash');
+$('.money').on('click', function(){
+    $(this).on('click');
+    $(this).off('click');
+    cash.load();
+    cash.play();
 });
 
 // Start the game blinking function //
