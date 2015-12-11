02674636cd03a8380af4aec3dd2fa9242ca7592a
brandonxjerome <jeromeyumang@live.com>
2015-12-11 00:01:05 -0500

    12/11/15
    I added a timer, but it's still not perfect. Also, a sound effect when
    the dollar bill is clicked, layout looks better.

--- game-assignment/js/main.js
+++ game-assignment/js/main.js
@@ -1,17 +1,18 @@
-// Money clicking function //
-
 $(document).ready(function() {
 
+//Money appearing in random positions
+
 var w = $(window).width();
-var h = $(window).width();
+var h = $(window).height();
 
     $('.money').each(function() {
-        $(this).css('left', Math.random() * w).css('top', Math.random() * h)
+    $(this).css('left', Math.random() * h).css('top', Math.random() * w)
     })
 
+    // Money clicking function //
 
    $('.money').click(function() {
-       $(this).toggle(600);
+    $(this).toggle(600);
 
    }); 
 
@@ -19,60 +20,65 @@ var h = $(window).width();
     // Level One text fade out //
 
     $(".l1").fadeOut(3000);
-
     $('a[data-page]').on('click', function(e) {
 
-        var go = $(this).attr('data-page');
-        $('.page').fadeOut(400).filter('#'+go).fadeIn(400);
+    var go = $(this).attr('data-page');
+    $('.page').fadeOut(400).filter('#'+go).fadeIn(400);
     });
 
-    
-
-
-
-});
+    });
 
 // Main Menu blinking function //
 
-$(function () {
+    $(function () {
     var $element = $('.begin');
     function fadeInOut () {
-        $element.fadeIn(700, function () {
-            $element.fadeOut(700, function () {
-                $element.fadeIn(700, function () {
-                    setTimeout(fadeInOut, 500);
-                });
-            });
-        });
+    $element.fadeIn(700, function () {
+    $element.fadeOut(700, function () {
+    $element.fadeIn(700, function () {
+    setTimeout(fadeInOut, 500);
+    });
+    });
+    });
     }
 
     fadeInOut();
-});
+    });
 
-var cash = document.getElementById('cash');
-$('.money').on('click', function(){
+//Money sound effect
+
+    var cash = document.getElementById('cash');
+    $('.money').on('click', function(){
     $(this).on('click');
     $(this).off('click');
     cash.load();
     cash.play();
-});
+    });
 
 // Start the game blinking function //
 
-$(function () {
+    $(function () {
     var $element = $('.start');
     function fadeInOut () {
-        $element.fadeIn(700, function () {
-            $element.fadeOut(700, function () {
-                $element.fadeIn(700, function () {
-                    setTimeout(fadeInOut, 500);
-                });
-            });
-        });
+    $element.fadeIn(700, function () {
+    $element.fadeOut(700, function () {
+    $element.fadeIn(700, function () {
+    setTimeout(fadeInOut, 500);
+    });
+    });
+    });
     }
 
     fadeInOut();
-});
+    });
+
+//Timer function
+
+    $("#time p").animate({width: "0"}, 5000, function(){
+    $("#time").fadeOut(300, function(){
+
+    });
+    });
 
 
 
