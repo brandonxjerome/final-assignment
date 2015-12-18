df450b8420dd5f6eda752570e8652c784ea7f041
brandonxjerome <jeromeyumang@live.com>
2015-12-17 22:25:23 -0500

    12/17/15
    Changed the background, and removed the icons for playing music.

--- game-assignment/js/main.js
+++ game-assignment/js/main.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
-//Money appearing in random positions
+    //Money appearing in random positions
 
-var w = $(window).width();
-var h = $(window).height();
+    var w = $(window).width();
+    var h = $(window).height();
 
     $('.money').each(function() {
     $(this).css('left', Math.random() * h).css('top', Math.random() * w)
@@ -12,23 +12,24 @@ var h = $(window).height();
     // Money clicking function //
 
    $('.money').click(function() {
-    $(this).toggle(600);
+    $(this).fadeOut(300);
 
    }); 
 
 
-    // Level One text fade out //
+    // Level One text fade out 
 
-    $(".l1").fadeOut(3000);
+    $(".l1").fadeOut(6000);
     $('a[data-page]').on('click', function(e) {
 
     var go = $(this).attr('data-page');
+
     $('.page').fadeOut(400).filter('#'+go).fadeIn(400);
-    });
 
     });
+    });
 
-// Main Menu blinking function //
+    // Main Menu blinking function 
 
     $(function () {
     var $element = $('.begin');
@@ -45,7 +46,7 @@ var h = $(window).height();
     fadeInOut();
     });
 
-//Money sound effect
+    // Money sound effect when clicked
 
     var cash = document.getElementById('cash');
     $('.money').on('click', function(){
@@ -55,7 +56,7 @@ var h = $(window).height();
     cash.play();
     });
 
-// Start the game blinking function //
+    // Start the game blinking function 
 
     $(function () {
     var $element = $('.start');
@@ -72,10 +73,10 @@ var h = $(window).height();
     fadeInOut();
     });
 
-//Timer function
+    // Timer function
 
     $("#time p").animate({width: "0"}, 5000, function(){
-    $("#time").fadeOut(300, function(){
+    $("#time").fadeOut(0, function(){
 
     });
     });
